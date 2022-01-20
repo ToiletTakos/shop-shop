81d882dd6f25d277c046a3dc5938c1a08385e7a5
Dan <dan.pandolfo@gmail.com>
2022-01-19 22:05:22 -0500

    created offline capabilities for view, and updating cart, and when re connecting

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,6 +14,8 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 import Cart from '../components/Cart';
 
+import { idbPromise } from '../utils/helpers';
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
@@ -25,16 +27,31 @@ function Detail() {
   const { products, cart } = state;
 
   useEffect(() => {
+    //already in global state
     if(products.length) {
       setCurrentProduct(products.find(product => product._id === id));
     }
+    // retrieved from server
     else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      })
     }
-  }, [products, data, dispatch, id]);
+    //get cache from idb
+    else if(!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
   
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id);
@@ -45,12 +62,20 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+
+      //if we're updating quantity, use existing item data and incremebt purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     }
     else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in indexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -59,6 +84,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    //upon removal from cart, delete the item from IndexedDb using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
